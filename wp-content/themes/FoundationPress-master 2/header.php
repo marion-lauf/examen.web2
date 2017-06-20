a32e857d60325b302af87a7e558f24ff5290f865
marion-lauf <marion.lauf@gmail.com>
2017-06-20 09:04:06 +0200

    Etape 4
    Structure page HTML

--- wp-content/themes/FoundationPress-master 2/header.php
+++ wp-content/themes/FoundationPress-master 2/header.php
@@ -14,6 +14,8 @@
 	<head>
 		<meta charset="<?php bloginfo( 'charset' ); ?>" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<link href="https://fonts.googleapis.com/css?family=Lora|Source+Sans+Pro" rel="stylesheet">
+
 		<?php wp_head(); ?>
 	</head>
 	<body <?php body_class(); ?>>
