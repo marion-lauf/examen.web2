e994680743cb2c8ef23fbb1b373adc7baa24c359
marion-lauf <marion.lauf@gmail.com>
2017-06-20 09:11:16 +0200

    Etape 5
    Ajout du script permettant d’actualiser automatiquement la page lors
    d’une modification dans le code

--- wp-content/themes/FoundationPress-master 2/header.php
+++ wp-content/themes/FoundationPress-master 2/header.php
@@ -37,14 +37,14 @@
 				</span>
 			</div>
 		</div>
+
+
 		<nav class="site-navigation top-bar" role="navigation">
 			<div class="top-bar-left">
 				<div class="site-desktop-title top-bar-title">
 					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
 				</div>
 			</div>
-			<div class="top-bar-right">
-				<?php foundationpress_top_bar_r(); ?>
 
 				<?php if ( ! get_theme_mod( 'wpt_mobile_menu_layout' ) || get_theme_mod( 'wpt_mobile_menu_layout' ) === 'topbar' ) : ?>
 					<?php get_template_part( 'template-parts/mobile-top-bar' ); ?>
