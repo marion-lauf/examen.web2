6eb0d45994fd3214179040b9076fa7c59dd72183
marion-lauf <marion.lauf@gmail.com>
2017-06-20 09:46:43 +0200

    Etape 6
    Pages HTML stylisées

--- wp-content/themes/FoundationPress-master 2/index.php
+++ wp-content/themes/FoundationPress-master 2/index.php
@@ -15,35 +15,44 @@
 
 get_header(); ?>
 
-<div class="main-wrap" role="main">
-	<article class="main-content">
-	<?php if ( have_posts() ) : ?>
 
-		<?php /* Start the Loop */ ?>
-		<?php while ( have_posts() ) : the_post(); ?>
-			<?php get_template_part( 'template-parts/content', get_post_format() ); ?>
-		<?php endwhile; ?>
-
-		<?php else : ?>
-			<?php get_template_part( 'template-parts/content', 'none' ); ?>
-
-		<?php endif; // End have_posts() check. ?>
-
-		<?php /* Display navigation to next/previous pages when applicable */ ?>
-		<?php
-		if ( function_exists( 'foundationpress_pagination' ) ) :
-			foundationpress_pagination();
-		elseif ( is_paged() ) :
-		?>
-			<nav id="post-nav">
-				<div class="post-previous"><?php next_posts_link( __( '&larr; Older posts', 'foundationpress' ) ); ?></div>
-				<div class="post-next"><?php previous_posts_link( __( 'Newer posts &rarr;', 'foundationpress' ) ); ?></div>
-			</nav>
-		<?php endif; ?>
-
-	</article>
-	<?php get_sidebar(); ?>
-
+<div class="row">
+	<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/slide.png" alt="image de presentation" class="slide"/>
+	<h1>des créations uniques</h1>
+	<a href="#features" class="button">Découvrir</a>
 </div>
+
+
+<div class="row">
+  <div class="large-3 columns">
+		<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/picto-diamant.png" alt="elements de rassurance" class="rassurance"/>
+		<p><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/puce.png" alt="puce" class="puce"/> Pièces uniques</p>
+		<p><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/puce.png" alt="puce" class="puce"/> Sur mesure</p>
+	</div>
+
+	<div class="large-3 columns">
+		<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/picto-madeinfrance.png" alt="elements de rassurance" class="rassurance"/>
+		<p><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/puce.png" alt="puce" class="puce"/> Fabrication Artisanale</p>
+		<p><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/puce.png" alt="puce" class="puce"/> 100% Française</p>
+	</div>
+
+
+  <div class="large-3 columns">
+		<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/picto-manuel.png" alt="elements de rassurance" class="rassurance"/>
+		<p><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/puce.png" alt="puce" class="puce"/> 6 ans d’expérience</p>
+		<p><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/puce.png" alt="puce" class="puce"/> Réalisations faites main</p>
+	</div>
+
+  <div class="large-3 columns">
+		<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/picto-qualite.png" alt="elements de rassurance" class="rassurance"/>
+		<p><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/puce.png" alt="puce" class="puce"/> Testés et approuvés</p>
+		<p><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/puce.png" alt="puce" class="puce"/> Matériaux nobles</p>
+	</div>
+</div>
+
+
+
+
+
 
 <?php get_footer();
